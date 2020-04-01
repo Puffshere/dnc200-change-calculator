cc2eaadbcb9ee722507aa2120125aea96d4c9efd
Shawn Taylor <shawncurtistaylor@hotmail.com>
2020-03-31 19:48:37 -0700

    second commit

--- src/dnc200-change-calculator/Program.cs
+++ src/dnc200-change-calculator/Program.cs
@@ -7,7 +7,24 @@ namespace dnc200_change_calculator
     {
         static void Main(string[] args)
         {
-            
+            decimal cost = 0;
+            decimal moneyGiven = 0;
+            Console.WriteLine("The price of the item is:  ");
+            string price = Console.ReadLine();
+            bool isCorrect = decimal.TryParse(price, out cost);
+            if (isCorrect)
+            {
+                Console.WriteLine(cost);
+            }
+            Console.WriteLine("Amount recieved for purchase of item:  ");
+            string amount = Console.ReadLine();
+            isCorrect = decimal.TryParse(amount, out moneyGiven);
+            if (isCorrect)
+            {
+                Console.WriteLine(moneyGiven);
+            }
+
+            Console.ReadKey();
         }
     }
 }
