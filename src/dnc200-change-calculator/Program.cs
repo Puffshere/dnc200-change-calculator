bc169eaf7111f651620acb482f3bebd77080ce53
Shawn Taylor <shawncurtistaylor@hotmail.com>
2020-03-31 22:02:49 -0700

    a

--- src/dnc200-change-calculator/Program.cs
+++ src/dnc200-change-calculator/Program.cs
@@ -16,39 +16,40 @@ namespace dnc200_change_calculator
 
             while (repeat)
             {
-            Console.Write("The price of the item is:  $" + price);
-            price = Console.ReadLine();
-            isCorrect = decimal.TryParse(price, out cost);
+                Console.Write("The price of the item is:  $" + price);
+                price = Console.ReadLine();
+                isCorrect = decimal.TryParse(price, out cost);
                 if (isCorrect)
                 {
-                Console.WriteLine("Price of the item accepted.");
-                repeat = false;
+                    Console.WriteLine("Price of the item accepted.");
+                    repeat = false;
                 }
                 else
                 {
-                Console.WriteLine("Please enter a valid amount.");
-                repeat = true;
+                    Console.WriteLine("Please enter a valid amount.");
+                    repeat = true;
                 }
             }
 
             while (!repeat)
             {
-            Console.Write("Amount recieved for purchase of item:  $");
-            amount = Console.ReadLine();
-            isCorrect = decimal.TryParse(amount, out moneyGiven);
+                Console.Write("Amount recieved for purchase of item:  $");
+                amount = Console.ReadLine();
+                isCorrect = decimal.TryParse(amount, out moneyGiven);
                 if (isCorrect)
                 {
-                Console.WriteLine("Amount given for puchase of the item is accepted.");
-                repeat = true;
+                    Console.WriteLine("Amount given for puchase of the item is accepted.");
+                    repeat = true;
                 }
                 else
                 {
-                Console.WriteLine("Please enter a valid amount.");
-                repeat = false;
+                    Console.WriteLine("Please enter a valid amount.");
+                    repeat = false;
                 }
             }
 
-            Console.ReadKey();
+            Console.WriteLine("Press any key to exit program . . .");
+            Console.Read();
         }
     }
 }
